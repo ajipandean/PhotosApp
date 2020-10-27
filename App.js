bdd6df992fdf8033ddb93e2323486c82c71d0b8e
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-10-27 13:33:11 +0800

    added camera roll package

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 24,
+  },
+  welcome: {
+    fontSize: 28,
+    marginBottom: 16,
+    fontWeight: 'bold',
   },
 });
+
+const alertActions = [
+  {
+    text: 'Cancel',
+    style: 'cancel',
+    onPress: () => console.log('Cancel button pressed!'),
+  },
+  {
+    text: 'Done',
+    onPress: () => console.log('Done button pressed!'),
+  },
+]
+
+export default function App() {
+  const technoAlert = () => {
+    Alert.alert(
+      "Technopreneurship Campus",
+      "Primakara Luar Biasa!",
+      alertActions,
+      { cancelable: true }
+    )
+  }
+
+  return (
+    <View style={[styles.container]}>
+      <View style={{ padding: 16 }}>
+        <Text style={[styles.welcome]}>
+          Welcome to Photos App
+        </Text>
+        <Button
+          title={"Show alert"}
+          onPress={technoAlert}
+        />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
